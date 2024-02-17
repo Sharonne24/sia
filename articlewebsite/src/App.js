b59b39cc583e2102931fc745fccb0fe92334a950
Sharonne24 <sawuor395@gmail.com>
2024-02-17 15:21:46 +0300

    app.js

--- articlewebsite/src/App.js
+++ articlewebsite/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import BlogDetail from './components/BlogDetail';
+import Portfolio from './components/Portfolio';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+
+        <main className="flex-grow">
+          <Switch>
+            <Route path="/home" exact component={Home} />
+            <Route path="/blog" component={BlogDetail} />
+            <Route path="/portfolio" component={Portfolio} />
+          </Switch>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
