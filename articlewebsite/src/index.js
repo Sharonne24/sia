f4757c97be9699df6844e6a455be8f70c2b6ce65
Sharonne24 <sawuor395@gmail.com>
2024-02-19 20:59:08 +0300

    change

--- articlewebsite/src/index.js
+++ articlewebsite/src/index.js
@@ -15,10 +15,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
+
 
 // ReactDOM.render(
 //   <React.StrictMode>
