df79cc0fca89fe2f04cf707e53bfd94cff3b07d8
Sharonne24 <sawuor395@gmail.com>
2024-02-19 21:17:14 +0300

    footer

--- articlewebsite/src/components/Footer.jsx
+++ articlewebsite/src/components/Footer.jsx
@@ -1,13 +1,11 @@
-// Footer.js
 import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      <h2>Footer</h2>
-      {/* Add your content here */}
-    </div>
-  );
-};
-
-export default Footer;
+    <footer className="bg-blue-500 text-white p-4">
+      <div className="flex items-center justify-center mb-4">
+        {/* Your Blog Logo */}
+        <img src="/path/to/your/logo.png" alt="Blog Logo" className="mr-2" />
+        {/* Your Blog Name */}
+        <h2 className="text-lg font-bold">Your Blog Name</h2>
+      </div>
