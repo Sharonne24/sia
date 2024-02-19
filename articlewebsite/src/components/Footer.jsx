eaecf276b22a2170bae65169d3de34b0edb25fce
Sharonne24 <sawuor395@gmail.com>
2024-02-19 21:18:52 +0300

    blog name

--- articlewebsite/src/components/Footer.jsx
+++ articlewebsite/src/components/Footer.jsx
@@ -4,8 +4,9 @@ const Footer = () => {
   return (
     <footer className="bg-blue-500 text-white p-4">
       <div className="flex items-center justify-center mb-4">
-        {/* Your Blog Logo */}
+        {/*Blog Logo */}
         <img src="/path/to/your/logo.png" alt="Blog Logo" className="mr-2" />
-        {/* Your Blog Name */}
+        {/* Blog Name */}
         <h2 className="text-lg font-bold">Your Blog Name</h2>
       </div>
+      <p className="text-sm">@ Created by Your Name</p>
