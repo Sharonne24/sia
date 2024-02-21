f8beefb629f9b6f833de1578adac450a52f423c4
Sharonne24 <sawuor395@gmail.com>
2024-02-21 21:29:44 +0300

    home

--- articlewebsite/src/components/Home.jsx
+++ articlewebsite/src/components/Home.jsx
@@ -1,35 +1,6 @@
-import React from 'react';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import BlogCard from './BlogCard';
 
-
-const Home = ({ categories }) => {
+const Home = () => {
   const [blogs, setBlogs] = useState([]);
-
-  useEffect(() => {
-    // Fetch blogs from your API or any other data source
-    // For now, let's assume you have a function to fetch blogs
-    const fetchBlogs = async () => {
-      try {
-        const response = await fetch('api_endpoint_blogs');
-        const data = await response.json();
-        setBlogs(data);
-      } catch (error) {
-        console.error('Error fetching blogs:', error);
-      }
-    };
-
-    // Call the fetchBlogs function
-    fetchBlogs();
-  }, []);
-
-  const topPostsBasedOnLikes = blogs
-    .slice() // Create a copy of blogs array to avoid modifying the original
-    .sort((a, b) => b.likes - a.likes) // Sort by likes in descending order
-    .slice(0, 5); // Get the top 5 posts
-
-    
-
-
-
-export default Home;
