8b260214cef45f3a97bd9ba9e35b080154fd9663
Sharonne24 <sawuor395@gmail.com>
2024-02-27 16:56:03 +0300

    traffic

--- articlewebsite/src/components/Dashboard.jsx
+++ articlewebsite/src/components/Dashboard.jsx
@@ -1,4 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-const AdminDashboard = () => {
+const Dashboard = () => {
   const [websiteTraffic, setWebsiteTraffic] = useState(0);
+
+  useEffect(() => {
+    const updateTraffic = () => {
+      setWebsiteTraffic((prevTraffic) => prevTraffic + 1);
+    };
+
+    // Simulate website traffic increase every 5 seconds
+    const interval = setInterval(updateTraffic, 5000);
